59dd24155b1a8c4f8db544fdafe73ceaa8cead1d
fidoedidoe <30518992+fidoedidoe@users.noreply.github.com>
2019-06-20 17:11:11 +0100

    Patch files before build

--- build_aosp_7-1_tilapia.sh
+++ build_aosp_7-1_tilapia.sh
@@ -4,11 +4,13 @@ WORK_DIRECTORY="$HOME/android/tilapia-aosp-7.1"
 REPO_DIRECTORY='.repo'
 REPO_SYNC_THREADS=$(nproc --all)
 #REPO_SYNC_THREADS="1"
-AOSP_REVISION="ads-7.1.0"
+#AOSP_REVISION="ads-7.1.0"
+AOSP_REVISION="test-7.1.0"
 REPO_INIT_FLAGS="--depth=1 --no-clone-bundle"
 #REPO_INIT_FLAGS="--no-clone-bundle"
 REPO_SYNC_FLAGS="--no-tags --no-clone-bundle"
 #REPO_SYNC_FLAGS="--no-clone-bundle"
+SLEEP_DURATION="1"
 
 PROMPT=""
 read -r -p "### (1/4) Start Repo Sync and Build process, first 'repo init' will take hours <Y/n>? (automatically continues unprompted after 5 seconds): " -t 5 -e -i Y PROMPT
@@ -30,8 +32,8 @@ cd "$WORK_DIRECTORY" || exit
 
 if [ ! -d "$WORK_DIRECTORY/$REPO_DIRECTORY" ]; then
   echo "### initialising AOSP repo for first time..."
-  repo init -u https://github.com/AndDiSa/platform_manifest-Grouper-AOSP.git -b $AOSP_REVISION $REPO_INIT_FLAGS
-  #repo init -u https://github.com/fidoedidoe/platform_manifest-Grouper-AOSP.git -b $AOSP_REVISION $REPO_INIT_FLAGS
+  #repo init -u https://github.com/AndDiSa/platform_manifest-Grouper-AOSP.git -b $AOSP_REVISION $REPO_INIT_FLAGS
+  repo init -u https://github.com/fidoedidoe/platform_manifest-Grouper-AOSP.git -b $AOSP_REVISION $REPO_INIT_FLAGS
 else
   echo "### LOS Repo exists..."
   echo "### step 1/1: reverting all local AOSP modifications..."
@@ -51,38 +53,239 @@ if [[ ! $PROMPT =~ ^[Yy]$ ]]; then
 fi
 
 echo "### sync repo with $REPO_SYNC_THREADS threads..."
-repo sync --quiet --force-broken --jobs="$REPO_SYNC_THREADS" $REPO_SYNC_FLAGS
+#repo sync --quiet --jobs="$REPO_SYNC_THREADS" $REPO_SYNC_FLAGS
+repo sync --quiet --force-sync --jobs="$REPO_SYNC_THREADS" $REPO_SYNC_FLAGS
+#repo sync --quiet --force-broken --jobs="$REPO_SYNC_THREADS" $REPO_SYNC_FLAGS
 
-#PROMPT=""
-#read -r -p "### (3/6) Continue and apply device specific patch <Y/n>? (automatically continues unprompted after 10 seconds): " -t 10 -e -i Y PROMPT
-#echo
-#if [ -z "$PROMPT" ]; then
-#  PROMPT="Y"
-#fi
-#if [[ ! $PROMPT =~ ^[Yy]$ ]]; then
-#  echo "### Response: '$PROMPT', exiting!"
-#  exit 1
-#fi
+PROMPT=""
+read -r -p "### (3/6) Continue and apply device specific patch <Y/n>? (automatically continues unprompted after 10 seconds): " -t 10 -e -i Y PROMPT
+echo
+if [ -z "$PROMPT" ]; then
+  PROMPT="Y"
+fi
+if [[ ! $PROMPT =~ ^[Yy]$ ]]; then
+  echo "### Response: '$PROMPT', exiting!"
+  exit 1
+fi
 
-#Patch script  - nolonger needed
-#cd "$WORK_DIRECTORY"/build || exit
-#patch -p 1 < ../.repo/manifests/build.patch
-#cd "$WORK_DIRECTORY"/frameworks/av || exit
-#patch -p 1 < ../../.repo/manifests/frameworks_av.patch
-#cd "$WORK_DIRECTORY"/frameworks/base || exit
-#patch -p 1 < ../../.repo/manifests/frameworks_base.patch
-#cd "$WORK_DIRECTORY"/frameworks/native || exit
-#patch -p 1 < ../../.repo/manifests/frameworks_native.patch
-#cd "$WORK_DIRECTORY"/hardware/ril || exit
-#patch -p 1 < ../../.repo/manifests/hardware_ril.patch
-##cd "$WORK_DIRECTORY"/packages/apps/Music || exit
-##patch -p 1 < ../../../.repo/manifests/packages_apps_music.patch
-#cd "$WORK_DIRECTORY"/system/core || exit
-#patch -p 1 < ../../.repo/manifests/system_core.patch
-#cd "$WORK_DIRECTORY"/system/sepolicy || exit
-#patch -p 1 < ../../.repo/manifests/system_sepolicy.patch
+#Patch script  - not needed if using forked repos
+cd "$WORK_DIRECTORY"/bionic || exit
+echo "### applying patch .repo/manifests/bionic.patch"
+patch -p 1 < ../.repo/manifests/bionic.patch
+sleep $SLEEP_DURATION
 
-#cd "$WORK_DIRECTORY" || exit
+cd "$WORK_DIRECTORY"/build || exit
+echo "### applying patch .repo/manifests/build.patch"
+patch -p 1 < ../.repo/manifests/build.patch
+sleep $SLEEP_DURATION
+
+cd "$WORK_DIRECTORY"/external/aac || exit
+echo "### applying patch .repo/manifests/external_aac.patch"
+patch -p 1 < ../../.repo/manifests/external_aac.patch
+sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/external/android-clat || exit
+echo "### applying patch .repo/manifests/external_android_clat.patch"
+patch -p 1 < ../../.repo/manifests/external_android_clat.patch
+sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/external/apache-http || exit
+echo "### applying patch .repo/manifests/external_apache-http.patch"
+patch -p 1 < ../../.repo/manifests/external_apache-http.patch
+sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/external/chromium-libpac || exit
+echo "### applying patch .repo/manifests/external_chromium_libpac.patch"
+patch -p 1 < ../../.repo/manifests/external_chromium_libpac.patch
+sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/external/curl || exit
+echo "### applying patch .repo/manifests/external_curl.patch"
+patch -p 1 < ../../.repo/manifests/external_curl.patch
+sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/external/e2fsprogs || exit
+echo "### applying patch .repo/manifests/external_e2fsprogs.patch"
+patch -p 1 < ../../.repo/manifests/external_e2fsprogs.patch
+sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/external/libavc || exit
+echo "### applying patch .repo/manifests/external_libavc.patch"
+patch -p 1 < ../../.repo/manifests/external_libavc.patch
+sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/external/libdrm || exit
+echo "### applying patch .repo/manifests/external_libdrm.patch"
+patch -p 1 < ../../.repo/manifests/external_libdrm.patch
+sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/external/libhevc || exit
+echo "### applying patch ./repo/manifests/external_libhevc.patch"
+patch -p 1 < ../../.repo/manifests/external_libhevc.patch
+sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/external/libmpeg2 || exit
+echo "### applying patch .repo/manifests/external_libmpeg2.patch"
+patch -p 1 < ../../.repo/manifests/external_libmpeg2.patch
+sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/external/libnfc-nci || exit
+echo "### applying patch .repo/manifests/external_libnfc-nci.patch"
+patch -p 1 < ../../.repo/manifests/external_libnfc-nci.patch
+sleep $SLEEP_DURATION
+# ######################################################################
+# This next patch fails, it has already need applied in AndDiSa's repo!!
+# ######################################################################
+#cd "$WORK_DIRECTORY"/external/libnfc-nxp || exit
+#echo "### applying patch .repo/manifests/external_libnfc-nxp.patch"
+#patch -p 1 < ../../.repo/manifests/external_libnfc-nxp.patch
+#sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/external/libvpx || exit
+echo "### applying patch .repo/manifests/external_libvpx.patch"
+patch -p 1 < ../../.repo/manifests/external_libvpx.patch
+sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/external/neven || exit
+echo "### applying patch .repo/manifests/external_neven.patch"
+patch -p 1 < ../../.repo/manifests/external_neven.patch
+sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/external/opencv || exit
+echo "### applying patch .repo/manifests/external_opencv.patch"
+patch -p 1 < ../../.repo/manifests/external_opencv.patch
+sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/external/skia || exit
+echo "### applying patch .repo/manifests/external_skia.patch"
+patch -p 1 < ../../.repo/manifests/external_skia.patch
+sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/external/sonivox || exit
+echo "### applying patch .repo/manifests/external_sonivox.patch"
+patch -p 1 < ../../.repo/manifests/external_sonivox.patch
+sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/external/sqlite || exit
+echo "### applying patch .repo/manifests/external_sqlite.patch"
+patch -p 1 < ../../.repo/manifests/external_sqlite.patch
+sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/external/svox || exit
+echo "### applying patch .repo/manifests/external_svox.patch"
+patch -p 1 < ../../.repo/manifests/external_svox.patch
+sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/external/tremolo || exit
+echo "### applying patch .repo/manifests/external_tremolo.patch"
+patch -p 1 < ../../.repo/manifests/external_tremolo.patch
+sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/external/wpa_supplicant_8 || exit
+echo "### applying patch .repo/manifests/external_wpa_supplicant_8.patch"
+patch -p 1 < ../../.repo/manifests/external_wpa_supplicant_8.patch
+sleep $SLEEP_DURATION
+
+cd "$WORK_DIRECTORY"/frameworks/av || exit
+echo "### applying patch .repo/manifests/frameworks_av.patch"
+patch -p 1 < ../../.repo/manifests/frameworks_av.patch
+sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/frameworks/base || exit
+echo "### applying patch .repo/manifests/frameworks_base.patch"
+patch -p 1 < ../../.repo/manifests/frameworks_base.patch
+sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/frameworks/ex || exit
+echo "### applying patch .repo/manifests/frameworks_ex.patch"
+patch -p 1 < ../../.repo/manifests/frameworks_ex.patch
+sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/frameworks/minikin || exit
+echo "### applying patch .repo/manifests/frameworks_minikin.patch"
+patch -p 1 < ../../.repo/manifests/frameworks_minikin.patch
+sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/frameworks/native || exit
+echo "### applying patch .repo/manifests/frameworks_native.patch"
+patch -p 1 < ../../.repo/manifests/frameworks_native.patch
+sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/frameworks/opt/net/wifi || exit
+echo "### applying patch .repo/manifests/frameworks_op_net_wifi.patch"
+patch -p 1 < ../../../../.repo/manifests/frameworks_op_net_wifi.patch
+sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/frameworks/support || exit
+echo "### applying patch .repo/manifests/frameworks_support.patch"
+patch -p 1 < ../../.repo/manifests/frameworks_support.patch
+sleep $SLEEP_DURATION
+
+cd "$WORK_DIRECTORY"/hardware/broadcom/wlan || exit
+echo "### applying patch .repo/manifests/hardware_broadcom_wlan.patch"
+patch -p 1 < ../../../.repo/manifests/hardware_broadcom_wlan.patch
+sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/hardware/libhardware || exit
+echo "### applying patch .repo/manifests/hardware_libhardware.patch"
+patch -p 1 < ../../.repo/manifests/hardware_libhardware.patch
+sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/hardware/qcom/audio || exit
+echo "### applying patch .repo/manifests/hardware_qcom_audio.patch"
+patch -p 1 < ../../../.repo/manifests/hardware_qcom_audio.patch
+sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/hardware/ril || exit
+echo "### applying patch .repo/manifests/hardware_ril.patch"
+patch -p 1 < ../../.repo/manifests/hardware_ril.patch
+sleep $SLEEP_DURATION
+
+cd "$WORK_DIRECTORY"/libcore || exit
+echo "### applying patch .repo/manifests/libcore.patch"
+patch -p 1 < ../.repo/manifests/libcore.patch
+sleep $SLEEP_DURATION
+
+cd "$WORK_DIRECTORY"/packages/apps/Email || exit
+echo "### applying patch .repo/manifests/packages_apps_email.patch"
+patch -p 1 < ../../../.repo/manifests/packages_apps_email.patch
+sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/packages/apps/Messaging || exit
+echo "### applying patch .repo/manifests/packages_apps_messaging.patch"
+patch -p 1 < ../../../.repo/manifests/packages_apps_messaging.patch
+sleep $SLEEP_DURATION
+######################################################################
+# The source directory for this patch doesn't exist, skipping patch ##
+######################################################################
+#cd "$WORK_DIRECTORY"/packages/apps/Music || exit
+#echo "### applying patch .repo/manifests/packages_apps_music.patch"
+#patch -p 1 < ../../../.repo/manifests/packages_apps_music.patch
+#sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/packages/apps/Nfc || exit
+echo "### applying patch .repo/manifests/packages_apps_nfc.patch"
+patch -p 1 < ../../../.repo/manifests/packages_apps_nfc.patch
+sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/packages/apps/Settings || exit
+echo "### applying patch .repo/manifests/packages_apps_settings.patch"
+patch -p 1 < ../../../.repo/manifests/packages_apps_settings.patch
+sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/packages/apps/UnifiedEmail || exit
+echo "### applying patch .repo/manifests/packages_apps_unifiedemail.patch"
+patch -p 1 < ../../../.repo/manifests/packages_apps_unifiedemail.patch
+sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/packages/providers/DownloadProvider || exit
+echo "### applying patch .repo/manifests/packages_provider_downloadprovider.patch"
+patch -p 1 < ../../../.repo/manifests/packages_provider_downloadprovider.patch
+sleep $SLEEP_DURATION
+
+####################################################################
+# Next Patch appears to have been applied (unlegacy project repo) ##
+####################################################################
+#cd "$WORK_DIRECTORY"/system/bt ||dd exit
+#echo "### applying patch .repo/manifests/system_bt.patch"
+#patch -p 1 < ../../.repo/manifests/system_bt.patch
+#sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/system/core ||dd exit
+echo "### applying patch .repo/manifests/system_core.patch"
+patch -p 1 < ../../.repo/manifests/system_core.patch
+sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/system/media ||dd exit
+echo "### applying patch .repo/manifests/system_media.patch"
+patch -p 1 < ../../.repo/manifests/system_media.patch
+sleep $SLEEP_DURATION
+###################################
+# Zero byte patch file, skipping ##
+###################################
+#cd "$WORK_DIRECTORY"/system/security ||dd exit
+#echo "### applying patch .repo/manifests/system_security.patch"
+#patch -p 1 < ../../.repo/manifests/system_security.patch
+#sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/system/sepolicy || exit
+echo "### applying patch .repo/manifests/system_sepolicy.patch"
+patch -p 1 < ../../.repo/manifests/system_sepolicy.patch
+sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/system/vold ||dd exit
+echo "### applying patch .repo/manifests/system_vold.patch"
+patch -p 1 < ../../.repo/manifests/system_vold.patch
+sleep $SLEEP_DURATION
+cd "$WORK_DIRECTORY"/system/update_engine ||dd exit
+echo "### applying patch .repo/manifests/update_engine.patch"
+patch -p 1 < ../../.repo/manifests/update_engine.patch
+echo "### End of patches!"
+
+cd "$WORK_DIRECTORY" || exit
 
 
 PROMPT=""
@@ -102,6 +305,9 @@ lunch aosp_tilapia-userdebug
 
 echo "### running croot..."
 croot
+
+echo "### set environment var to stop issues with prebuilts/misc/.../flex"
+export LC_ALL=C
 
 echo "### remove previous build output"
 rm -rf out/*
